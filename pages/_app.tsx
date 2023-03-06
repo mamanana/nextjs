04ef924135e633315327a63c56fcec9a86fb3d2a
hung <quochung@simicart.com>
2023-03-07 01:11:17 +0700

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/tailwind.css'
 import type { AppProps } from 'next/app'
+import apolloClient from '../lib/apollo'
+import { ApolloProvider } from '@apollo/client'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
