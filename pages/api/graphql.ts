e728c7b81a6681d307e9e02ca22f9ce8faab0b9f
hung <quochung@simicart.com>
2023-03-08 18:25:35 +0700

    add database

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,13 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { createSchema, createYoga } from 'graphql-yoga'
+import { resolvers } from '../../graphql/resolvers'
+import { typeDefs } from '../../graphql/schema'
 
-type Data = {
-  name: string
-}
+export default createYoga<{
+  req: NextApiRequest
+  res: NextApiResponse
+}>({
+  schema: createSchema({
+    typeDefs,
+    resolvers
+  }),
+  graphqlEndpoint: '/api/graphql'
+})
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export const config = {
+  api: {
+    bodyParser: false
+  }
 }
