81d5193c064140c5ea22ad39a14521ef74500bec
hung <quochung@simicart.com>
2023-03-10 18:37:01 +0700

    update

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,33 +1,16 @@
-// /graphql/resolvers.ts
-export const resolvers = {
-    Query: {
-      links: () => {
-        return [
-          {
-            category: 'Open Source',
-            description: 'Fullstack React framework',
-            id: 1,
-            imageUrl: 'https://nextjs.org/static/twitter-cards/home.jpg',
-            title: 'Next.js',
-            url: 'https://nextjs.org',
-          },
-          {
-            category: 'Open Source',
-            description: 'Next Generation ORM for TypeScript and JavaScript',
-            id: 2,
-            imageUrl: 'https://www.prisma.io/images/og-image.png',
-            title: 'Prisma',
-            url: 'https://www.prisma.io',
-          },
-          {
-            category: 'Open Source',
-            description: 'GraphQL implementation',
-            id: 3,
-            imageUrl: 'https://www.apollographql.com/apollo-home.jpg',
-            title: 'Apollo GraphQL',
-            url: 'https://apollographql.com',
-          },
-        ]
-      },
-    },
-  }
+import path from 'path'
+import { mergeResolvers } from '@graphql-tools/merge'
+import { loadFilesSync } from '@graphql-tools/load-files'
+
+// import linkResolver from '../graphql/resolvers/links'
+
+const rootDirectory = process.cwd();
+
+// const linkResolver = require(path.join(rootDirectory, '/graphql/resolvers'))
+
+
+const resolversArray = loadFilesSync(path.join(rootDirectory, '/graphql/resolvers'), { extensions: ['ts'] })
+
+const resolvers = mergeResolvers(resolversArray)
+
+export default resolvers
--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,8 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { createSchema, createYoga } from 'graphql-yoga'
-import { resolvers } from '../../graphql/resolvers'
-import { typeDefs } from '../../graphql/schema'
+import resolvers from '../../graphql/resolvers'
+import typeDefs from '../../graphql/typeDefs'
 
 export default createYoga<{
   req: NextApiRequest
