0270544b71f1e9a745709949920c09caf38dbc86
hung <quochung@simicart.com>
2023-03-13 17:15:23 +0700

    update

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,16 +1,9 @@
-import path from 'path'
-import { mergeResolvers } from '@graphql-tools/merge'
-import { loadFilesSync } from '@graphql-tools/load-files'
+import Links from './resolvers/links'
 
-// import linkResolver from '../graphql/resolvers/links'
-
-const rootDirectory = process.cwd();
-
-// const linkResolver = require(path.join(rootDirectory, '/graphql/resolvers'))
-
-
-const resolversArray = loadFilesSync(path.join(rootDirectory, '/graphql/resolvers'), { extensions: ['ts'] })
-
-const resolvers = mergeResolvers(resolversArray)
+const resolvers = {
+  Query: {
+    ...Links
+  },
+};
 
 export default resolvers
